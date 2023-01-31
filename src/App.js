e8783f9f1c96787279205842e20401e81444f7cc
Judy Kong <judy.kong817@gmail.com>
2023-01-31 09:13:01 -0500

    Async Partners

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AboutPage from './pages/AboutPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
 import './App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
   }, [dispatch]);
 
   return (
